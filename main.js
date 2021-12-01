1bdbffcd0df65ba0faa6c97bdd5b82dc80f4838e
Anshika0707 <83493340+Anshika0707@users.noreply.github.com>
2021-12-01 13:50:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
-        noseX = results[0].pose.nose.x;
+        noseX = results[0].pose.nose.x-40;
         noseY = results[0].pose.nose.y;
-        console.log("nose x = " + results[0].pose.nose.x+40);
-        console.log("nose y = " + results[0].pose.nose.y );
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
 
     }
 }
